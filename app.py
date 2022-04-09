7d67b34f1cedecad2533175e55ace07762943308
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-04-09 13:53:17 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+dcoderImg=''
+st.title("<raf /> Blog")
+st.header("Cara Install Streamlit di Dcoder")
+st.write("Untuk menginstall Streamlit di Dcoder langkah pertama yang harus dilakuakan adalah menginstall aplikasi Dcoder melalui Google Play Store")
+st.image(dcoderImg)
+link = '[Install Dcoder](https://play.google.com/store/apps/details?id=com.paprbit.dcoder)'
+st.markdown(link, unsafe_allow_html=True)
