7471fa716d577f173f1323ef045be951e4dc24d6
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-04-12 01:53:39 +0000

    ..

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 import streamlit as st
-dcoderImg='https://raw.githubusercontent.com/raf-ar/raf-st/main/public/img/IMG_20220409_120731.jpg'
-st.title("<raf /> Blog")
-st.header("Cara Install Streamlit di Dcoder")
-st.write("Untuk menginstall Streamlit di Dcoder langkah pertama yang harus dilakuakan adalah menginstall aplikasi Dcoder melalui Google Play Store")
-st.image(dcoderImg)
-link = '[Install Dcoder](https://play.google.com/store/apps/details?id=com.paprbit.dcoder)'
-st.markdown(link, unsafe_allow_html=True)
+import pandas as pd
+from datetime import date
+from data import siswa
+##definition
+now = date.today()
+##data
+st.title("Absen Aplication")
+st.header("X TKJ 3")
+form = st.form(key="my_form")
+tanggal = form.date_input("Tanggal", now)
+namaSiswa = form.multiselect("Absensi",
+                             options=siswa.nama,
+                             help="tentukan untuk siapa")
+keterangan = form.radio("keterangan",
+                        ('Hadir', 'Sakit', 'Izin', 'Tanpa Keterangan'))
+submitted = form.form_submit_button("Submit")
+data = {"tanggal":{tanggal.strftime("%d %B, %Y"): {"Siswa": namaSiswa, "Ket": keterangan}}}
+print(data)
+if (submitted):
+	data["tanggal"]:{tanggal:{"Siswa":namaSiswa,"Ket":keterangan}}}
+for k,v in data["tanggal"].items():
+	rekapAbsen = st.title('Rekap Absen')
+	st.write("Absen Tanggal :", k)
+	st.table(v)
