720a193c03da2044ca314abb4a97d9c9c21b7c29
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-04-16 10:36:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,10 +22,5 @@ rekapAbsen = st.title('Rekap Absen')
 
 if (submitted):
 	st.subheader("Absen Tanggal", tanggal.strftime("%d %B, %Y"))
-        dataRekap = {
-         "Nama": namaSiswa,
-         "Keterangan": keterangan
-         }
-
-
-st.table(dataRekap)
+        dataRekap = {"Nama": namaSiswa, "Keterangan": keterangan}
+        st.table(dataRekap)
