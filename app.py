ceb47d4010acf077f0d4ad095eca6fb2c0b87073
raf-ar <dev.rafaalrazzak@gmail.com>
2022-05-16 18:23:52 +0700

    .

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
+from asyncio.windows_events import NULL
 import streamlit as st
 import pandas as pd
 from datetime import date
 from data import siswa
-##definition
+# definition
 now = date.today()
-logo="https://raw.githubusercontent.com/raf-ar/raf-st/a4c5cb39868a9a6d0953e95aa60264a9eb7cf908/public/img/ambigram-v4.png"
-##data
+logo = "https://raw.githubusercontent.com/raf-ar/raf-st/a4c5cb39868a9a6d0953e95aa60264a9eb7cf908/public/img/ambigram-v4.png"
 
-st.set_page_config(page_title="<raf />", page_icon=logo, layout="centered", initial_sidebar_state="auto", menu_items=None)
+dataRekap = {
+}
+
+
+def listToString(s):
+    str1 = ", "
+    return (str1.join(s))
+
+# data
+
+
+st.set_page_config(page_title="<raf />", page_icon=logo,
+                   layout="centered", initial_sidebar_state="auto", menu_items=None)
 st.title("Absen Aplication")
 st.header("X TKJ 3")
 form = st.form(key="my_form")
@@ -15,12 +27,19 @@ tanggal = form.date_input("Tanggal", now)
 namaSiswa = form.multiselect("Absensi",
                              options=siswa.nama,
                              help="tentukan untuk siapa")
-keterangan = form.radio("keterangan",
+keterangan = form.radio("Keterangan",
                         ('Hadir', 'Sakit', 'Izin', 'Tanpa Keterangan'))
 submitted = form.form_submit_button("Submit")
 rekapAbsen = st.title('Rekap Absen')
 
 if (submitted):
-  dataRekap = {"Nama": namaSiswa, "Keterangan": keterangan}
-  st.subheader("Absen Tanggal", tanggal.strftime("%d %B, %Y"))
-  st.table(dataRekap)
+    if(len (namaSiswa) != 0  or keterangan != NULL):
+        dataRekap = {"data": {
+            "Tanggal": tanggal, "Siswa": {
+                "Nama": namaSiswa, "Keterangan": keterangan
+            }
+        }}
+        st.subheader(tanggal.strftime("%d %b, %Y"))
+        st.subheader("Nama: ")
+        st.write(listToString(dataRekap["data"]["Siswa"]["Nama"]), ":",
+                 dataRekap["data"]["Siswa"]["Keterangan"])
